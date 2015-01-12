fd6093f53e3d820644c4f8d4cdf3da83b25b1645
pinguo-yuyidong <yuyidong@camera360.com>
2015-01-12 14:53:49 +0800

    Fix some bug

--- app/src/main/java/us/yydcdut/androidltest/callback/JpegSessionCallback.java
+++ app/src/main/java/us/yydcdut/androidltest/callback/JpegSessionCallback.java
@@ -38,7 +38,8 @@ public class JpegSessionCallback extends CameraCaptureSession.CaptureCallback {
         //聚焦完成才能拍照
         if (afState.intValue() == CameraMetadata.CONTROL_AF_STATE_FOCUSED_LOCKED || afState.intValue() == CameraMetadata.CONTROL_AF_STATE_NOT_FOCUSED_LOCKED) {
             Log.i("JpegSessionCallback", "进去了一层,,aeState.intValue()--->" + aeState.intValue());
-            if (aeState.intValue() == CameraMetadata.CONTROL_AE_STATE_LOCKED || aeState == CameraMetadata.CONTROL_AE_STATE_PRECAPTURE || aeState.intValue() == CameraMetadata.CONTROL_AE_STATE_FLASH_REQUIRED) {
+            if (aeState.intValue() == CameraMetadata.CONTROL_AE_STATE_LOCKED || aeState == CameraMetadata.CONTROL_AE_STATE_PRECAPTURE
+                    || aeState.intValue() == CameraMetadata.CONTROL_AE_STATE_FLASH_REQUIRED) {
                 Log.i("JpegSessionCallback", "进去了两层");
                 mHandler.post(new Runnable() {
                     @Override
